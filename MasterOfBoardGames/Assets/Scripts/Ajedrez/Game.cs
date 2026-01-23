23475e1de8f2f88eb527bb6b41a90c2c47681918
MamadiMD <mmagassdam1@educacion.navarra.es>
2026-01-23 13:38:18 +0100

    [MINIUEGOS]: Arreglos en el codigo del juuego de ajedrez , ya funciona el ajedrez en 1vs1 pero falta por implementar la cpu , las instrucciones y las opciones

--- MasterOfBoardGames/Assets/Scripts/Ajedrez/Game.cs
+++ MasterOfBoardGames/Assets/Scripts/Ajedrez/Game.cs
@@ -1,103 +1,84 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
-    public GameObject pieza;
+    //Reference from Unity IDE
+    public GameObject chesspiece;
 
-    private GameObject [,] position = new GameObject[8,8];
-    private GameObject [] piezasNegras = new GameObject[16];
-    private GameObject [] piezasBlancas = new GameObject[16];
+    //Matrices needed, positions of each of the GameObjects
+    //Also separate arrays for the players in order to easily keep track of them all
+    //Keep in mind that the same objects are going to be in "positions" and "playerBlack"/"playerWhite"
+    private GameObject[,] positions = new GameObject[8, 8];
+    private GameObject[] playerBlack = new GameObject[16];
+    private GameObject[] playerWhite = new GameObject[16];
 
-    private string currentPlayer = "Blanco";
+    //current turn
+    private string currentPlayer = "white";
 
+    //Game Ending
     private bool gameOver = false;
 
-    void Start()
+    //Unity calls this right when the game starts, there are a few built in functions
+    //that Unity can call for you
+    public void Start()
     {
-        piezasBlancas = new GameObject[]
-        {
-            Create("Reina_Blanca", 3, 0),
-            Create("Rey_Blanco", 4, 0),
-            Create("Alfil_Blanco", 2, 0),
-            Create("Alfil_Blanco", 5, 0),
-            Create("Torre_Blanca", 0, 0),
-            Create("Torre_Blanca", 7, 0),
-            Create("Caballo_Blanco", 1, 0),
-            Create("Caballo_Blanco", 6, 0),
-            Create("Peon_Blanco", 0, 1),
-            Create("Peon_Blanco", 1, 1),
-            Create("Peon_Blanco", 2, 1),
-            Create("Peon_Blanco", 3, 1),
-            Create("Peon_Blanco", 4, 1),
-            Create("Peon_Blanco", 5, 1),
-            Create("Peon_Blanco", 6, 1),
-            Create("Peon_Blanco", 7, 1)
+        playerWhite = new GameObject[] { Create("white_rook", 0, 0), Create("white_knight", 1, 0),
+            Create("white_bishop", 2, 0), Create("white_queen", 3, 0), Create("white_king", 4, 0),
+            Create("white_bishop", 5, 0), Create("white_knight", 6, 0), Create("white_rook", 7, 0),
+            Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1),
+            Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
+            Create("white_pawn", 6, 1), Create("white_pawn", 7, 1) };
+        playerBlack = new GameObject[] { Create("black_rook", 0, 7), Create("black_knight",1,7),
+            Create("black_bishop",2,7), Create("black_queen",3,7), Create("black_king",4,7),
+            Create("black_bishop",5,7), Create("black_knight",6,7), Create("black_rook",7,7),
+            Create("black_pawn", 0, 6), Create("black_pawn", 1, 6), Create("black_pawn", 2, 6),
+            Create("black_pawn", 3, 6), Create("black_pawn", 4, 6), Create("black_pawn", 5, 6),
+            Create("black_pawn", 6, 6), Create("black_pawn", 7, 6) };
 
-        };
-
-        piezasNegras = new GameObject[]
+        //Set all piece positions on the positions board
+        for (int i = 0; i < playerBlack.Length; i++)
         {
-            Create("Reina_Negra", 3, 7),
-            Create("Rey_Negro", 4, 7), 
-            Create("Alfil_Negro", 2, 7),
-            Create("Alfil_Negro", 5, 7),
-            Create("Torre_Negra", 0, 7),
-            Create("Torre_Negra", 7, 7),
-            Create("Caballo_Negro", 1, 7),
-            Create("Caballo_Negro", 6, 7),
-            Create("Peon_Negro", 0, 6),
-            Create("Peon_Negro", 1, 6),
-            Create("Peon_Negro", 2, 6),
-            Create("Peon_Negro", 3, 6),
-            Create("Peon_Negro", 4, 6),
-            Create("Peon_Negro", 5, 6),
-            Create("Peon_Negro", 6, 6),
-            Create("Peon_Negro", 7, 6)
-        };
-
-        for(int i = 0 ; i < piezasNegras.Length ; i++)
-        {
-            SetPosition(piezasNegras[i]);
-            SetPosition(piezasBlancas[i]);
+            SetPosition(playerBlack[i]);
+            SetPosition(playerWhite[i]);
         }
     }
 
     public GameObject Create(string name, int x, int y)
     {
-        GameObject obj = Instantiate(pieza, new Vector3(0,0,-1), Quaternion.identity); 
-        Chessman cm = obj.GetComponent<Chessman>();
-        cm.name = name;
+        GameObject obj = Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
+        Chessman cm = obj.GetComponent<Chessman>(); //We have access to the GameObject, we need the script
+        cm.name = name; //This is a built in variable that Unity has, so we did not have to declare it before
         cm.SetXBoard(x);
         cm.SetYBoard(y);
-        cm.Activate();
+        cm.Activate(); //It has everything set up so it can now Activate()
         return obj;
     }
 
     public void SetPosition(GameObject obj)
     {
         Chessman cm = obj.GetComponent<Chessman>();
-        position[cm.GetXBoard(), cm.GetYBoard()] = obj;
+
+        //Overwrites either empty space or whatever was there
+        positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
     }
 
     public void SetPositionEmpty(int x, int y)
     {
-        position[x, y] = null;
+        positions[x, y] = null;
     }
 
     public GameObject GetPosition(int x, int y)
     {
-        return position[x, y];
+        return positions[x, y];
     }
 
     public bool PositionOnBoard(int x, int y)
     {
-        if(x < 0 || y < 0 || x >=  position.GetLength(0) || y >= position.GetLength(1))
-        {
-            return false;
-        }
+        if (x < 0 || y < 0 || x >= positions.GetLength(0) || y >= positions.GetLength(1)) return false;
         return true;
     }
 
@@ -113,23 +94,33 @@ public class Game : MonoBehaviour
 
     public void NextTurn()
     {
-        if (currentPlayer == "Blanco")
+        if (currentPlayer == "white")
         {
-            currentPlayer = "Negro";
+            currentPlayer = "black";
         }
         else
         {
-            currentPlayer = "Blanco";
+            currentPlayer = "white";
         }
     }
 
     public void Update()
     {
-        if(gameOver == true && Input.GetMouseButtonDown(0))
+        if (gameOver == true && Input.GetMouseButtonDown(0))
         {
             gameOver = false;
-            SceneManager.LoadScene("Ajedrez");
+
+            SceneManager.LoadScene("Ajedrez"); 
         }
     }
+    
+    public void Winner(string playerWinner)
+    {
+        gameOver = true;
 
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().enabled = true;
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().text = "A GANADO : " + playerWinner;
+
+        GameObject.FindGameObjectWithTag("RestartText").GetComponent<Text>().enabled = true;
+    }
 }
